c16dd0490ba69b6d05ae639197ceaf285f1acae4
MiguelLuceroSL <roquerobertomiguellucero@gmail.com>
2024-04-25 10:53:29 -0300

    ultimo cambio

--- index.js
+++ index.js
@@ -132,7 +132,6 @@ app.get('/productos', async (req, res) => {
                 //si no hay descuento igual le da el atributo pero en false
                 //para manejar en el front
             }
-            console.log(producto)
             return producto; //retorno el producto ya modificado del mapeo
         }));
 
